e74cd85c11df6d8d43f61a80161bf1c6bcbb5496
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 22:21:48 +0200

    Update fibonnaci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = +a;
+    
+    if(a === 0) return 0;
+    if(a < 0) return "OOPS";
 
+    let sequence = [1,1];
+
+    for(let i = 1; i <= a - 2; i++) {
+        sequence.push(sequence[i - 1] + sequence[i]);
+    }
+
+    return sequence[a - 1];
 };
 
 // Do not edit below this line
