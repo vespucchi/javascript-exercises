6966a128f1ec0a9a161f87048cc5c76e47a89a59
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 21:50:20 +0200

    Update palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const alphanum_regex = /[a-z0-9]/i;
+    
+    text = text
+        .toLowerCase()
+        .split("")
+        .filter(item => item.match(alphanum_regex));
 
+    let reversed = text.toReversed();
+
+    return text.join("").toString() === 
+        reversed.join("").toString() ? true : false;
 };
 
 // Do not edit below this line
