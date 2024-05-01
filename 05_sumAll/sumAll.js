c5e25caab2ac9ab2660bb19a6f7974764b538933
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 18:46:02 +0200

    Update sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if(!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return "ERROR";
+    } else if(firstNumber < 0 || secondNumber < 0) {
+        return "ERROR";
+    }
 
+    let num1 = Math.min(firstNumber, secondNumber);
+    let num2 = Math.max(firstNumber, secondNumber);
+    let sum = 0;
+
+    for(let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
