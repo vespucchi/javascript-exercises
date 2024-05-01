e5e98f6c12714902cdfc2e61e0ff292097a98c67
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 23:21:38 +0200

    Update findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
 
+        return oldestAge < currentAge ? current : oldest;
+    })
 };
+
+const getAge = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
