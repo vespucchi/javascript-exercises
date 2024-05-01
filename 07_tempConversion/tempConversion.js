cfbab2969ffaea3e66d2abb134d88fbca6a9c878
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 19:17:43 +0200

    Update tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  return Math.round((5/9 * (tempInF-32)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  return Math.round((9/5 * tempInC + 32) * 10) / 10;
 };
 
 // Do not edit below this line
