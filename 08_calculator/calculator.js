e1ec9cd7289f99c31b7ff9e30e04224fa790af2a
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 21:00:02 +0200

    Update calculator function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  if (a < b) [a, b] = [b, a];
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(item => sum += item);
+
+  // reduce(callbackFn, initialValue)
+  // array.reduce((total, current) => total + current, 0);
+
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
+  array.forEach(item => product *= item);
 
+  // reduce(callbackFn, initialValue)
+  // array.reduce((product, current) => product * current);
+
+	return product === 1 ? 0 : product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a === 0) return 1;
+  if(a === 1) return 1;
+
+  let product = 1;
+
+	for(let i = 2; i <= a; i++) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
