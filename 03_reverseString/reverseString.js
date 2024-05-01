e399ed32a0a0131c0ee4fb3cc8092c45113ee9d7
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-01 17:13:10 +0200

    Update reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let chars = text.split("");
+    let reversed = [];
 
+    for(let i = 0; i < text.length; i++) {
+        reversed.unshift(chars[i]);
+    }
+
+    return reversed.join("");
 };
 
 // Do not edit below this line
